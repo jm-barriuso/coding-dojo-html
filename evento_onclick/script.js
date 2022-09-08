1491ae0ade71e73f18fa19ebc873372186c302be
José Miguel Barriuso <josemiguelbarriuso@Joses-MacBook-Pro.local>
2022-09-07 23:11:29 -0400

    evento onclick

---
+++ evento_onclick/script.js
@@ -0,0 +1,11 @@
+function turnLogOut(element){
+    element.innerText = "Logout";
+    }
+
+function removeButton(element){
+    element.remove();
+}
+
+function sendAlert(){
+    alert('ninja was liked');
+}
