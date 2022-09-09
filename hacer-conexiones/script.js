8265ac9ac51d41b7009bf80541789873d190c0e3
José Miguel Barriuso <josemiguelbarriuso@Joses-MacBook-Pro.local>
2022-09-08 23:17:33 -0400

    hacer conexiones

---
+++ hacer-conexiones/script.js
@@ -0,0 +1,21 @@
+console.log("page loaded...");
+
+let cardName = document.getElementById("name-card");
+
+function changeName(){
+    console.log(cardName);
+    cardName.innerText = "Juana Perez"; 
+}
+
+let badgeMinus = document.querySelector(".badge");
+let badgePlus = document.getElementById("connections")
+function removeRequest(e,request){
+    e.parentNode.parentNode.parentNode.removeChild(e.parentNode.parentNode);
+    if(request === 'accept'){
+        badgePlus.innerText ++;
+    }
+    badgeMinus.innerText --;
+    console.log(request);
+
+
+}
